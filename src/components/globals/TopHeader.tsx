480eb7286e98d95930a19166035a07b5c51592d2
Your Name <you@example.com>
2022-06-09 21:16:07 +0100

    I added party popper emoji to Top header component

--- src/components/globals/TopHeader.tsx
+++ src/components/globals/TopHeader.tsx
@@ -5,7 +5,7 @@ const TopHeader = () => {
   return (
     <div className="bg-base flex justify-center items-center h-14">
       <div className=" px-5 text-sm mr-4 font-normal font-secondary text-white ">
-        Free: Registration for the cohort VII currently ongoing Apply
+        🎉Free: Registration for the cohort VII currently ongoing Apply
         <span className="underline ml-1 text-white10">
           <a href="https://google.com">here</a>
         </span>
