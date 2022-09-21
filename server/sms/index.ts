c9bb4631fb352ef2d45e25058c39dc5698582b1a
oke kehinde <okekehinde@gmail.com>
2022-09-21 10:38:12 +0100

    fixes

--- server/sms/index.ts
+++ server/sms/index.ts
@@ -1,19 +1,21 @@
 import axios from "axios"
 import {smsConfig} from "../config"
 
+const config = {
+    headers: { Authorization: `Bearer ${process.env.SMS_KEY}` }
+};
 export const sendSms = async (data: any={}) => {
     try {
         const smsData = {
             ...smsConfig,
-           ...data
-        }
-        const response = await axios.post(`https://app.multitexter.com/v2/app/sms`, smsData)
-
-        // console.log(response.data, 'ssms daeadta')
+           ...data,
         
+        }
+        const response = await axios.post(`https://app.multitexter.com/v2/app/sendsms`, smsData, config)
+        // console.log(response.data, 'ssms daeadta')
         return response
     } catch (error) {
-        console.log(error, "sms error")
+        // console.log(error, "sms error")
         return error
     }
 }
