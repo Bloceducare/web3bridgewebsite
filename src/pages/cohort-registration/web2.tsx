9460737dd609ab95a7605e1af17b282769d2f04b
Oke Kehinde <55279370+okeken@users.noreply.github.com>
2022-11-02 10:21:49 +0100

    Misc

--- src/pages/cohort-registration/web2.tsx
+++ src/pages/cohort-registration/web2.tsx
@@ -3,8 +3,8 @@ import { COHORT_REGISTRATION_OPENED } from "config/constant"
 
 const  Web2Registration = ()=>{
     return (<>
-    { COHORT_REGISTRATION_OPENED ?   <Web2View />:   <h1 className="font-bold text-center dark:text-white20 my-52">Registration has closed!!</h1>
-   }
+      <Web2View />
+   
   
     </>)
 }
