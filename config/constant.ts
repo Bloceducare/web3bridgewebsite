5c623a97dd31958214684b65e2240f64436fa80d
okeken <okekehinde@gmail.com>
2023-04-30 15:41:31 +0100

    ci/cd  addition

--- config/constant.ts
+++ config/constant.ts
@@ -1,40 +1,39 @@
 import { EspecializedOptions } from "enums";
 
 export const LOCATION_BASE_URL = "https://countriesnow.space/api/v0.1/";
-export const  COHORT_REGISTRATION_OPENED=true
-export const SPECIAL_CLASS_OPENED=true
+export const COHORT_REGISTRATION_OPENED = true;
+export const SPECIAL_CLASS_OPENED = true;
 
 export const TRAINING_CLOSED = {
-    web2:true,
-    web3:true,
-    specialClass:true,
-    cairo:false
-}
-
+  web2: true,
+  web3: true,
+  specialClass: true,
+  cairo: true,
+};
 
 export const specializedClassOptions = [
-    {label:"Html, CSS, intro to JavaScript", value:EspecializedOptions[0]},
-    {label:"JavaScript, react, typescript", value:EspecializedOptions[1]},
-    {label:"JavaScript, nodejs", value:EspecializedOptions[2]},
-    {label:"GO", value:EspecializedOptions[3]},
-    {label:"Solidity", value:EspecializedOptions[4]},
-]
+  { label: "Html, CSS, intro to JavaScript", value: EspecializedOptions[0] },
+  { label: "JavaScript, react, typescript", value: EspecializedOptions[1] },
+  { label: "JavaScript, nodejs", value: EspecializedOptions[2] },
+  { label: "GO", value: EspecializedOptions[3] },
+  { label: "Solidity", value: EspecializedOptions[4] },
+];
 
-export const trainingTime = [ 
-    {
-    label:"morning",
-    value:"0"
-},
-    {
-    label:"evening",
-    value:"1"
-},
-]
+export const trainingTime = [
+  {
+    label: "morning",
+    value: "0",
+  },
+  {
+    label: "evening",
+    value: "1",
+  },
+];
 
 // Html, CSS, intro to JavaScript
 
 // JavaScript, react, typescript
 
-// JavaScript, nodejs, 
+// JavaScript, nodejs,
 
 // GO
