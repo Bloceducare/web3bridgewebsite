90fd51dcbdf727f144acf9539e1127aed679fec9
codeWhizperer <Adegbiteademola1999@gmail.com>
2023-11-07 21:32:54 +0100

    fix error

--- config/constant.ts
+++ config/constant.ts
@@ -6,8 +6,8 @@ export const COHORT_REGISTRATION_OPENED = true;
 export const SPECIAL_CLASS_OPENED = true;
 export const registrationPaused = false;
 export const TRAINING_CLOSED = {
-  web2: isOpened,
-  web3: false,
+  web2: !isOpened,
+  web3: true,
   specialClass:false,
   cartesi: false,
 };
