0d6279476d5ac0c71bc88f0c0c6cef36de348a81
codeWhizperer <Adegbiteademola1999@gmail.com>
2023-12-09 21:00:48 +0100

    close registration

--- config/constant.ts
+++ config/constant.ts
@@ -7,7 +7,7 @@ export const SPECIAL_CLASS_OPENED = true;
 export const registrationPaused = false;
 export const TRAINING_CLOSED = {
   web2: isOpened,
-  web3: false,
+  web3: true,
   specialClass:false,
   cartesi: true,
 };
