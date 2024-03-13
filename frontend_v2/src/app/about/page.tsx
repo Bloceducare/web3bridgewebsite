5e16dec5799acc9488b2d34cdc89d683f1921c90
signor1 <emmanuelomemgboji@gmail.com>
2024-03-13 03:20:03 +0100

    chore: started about-us page

--- frontend_v2/src/app/about/page.tsx
+++ frontend_v2/src/app/about/page.tsx
@@ -1,3 +1,11 @@
+import { HeroSection } from "@/components/about/HeroSection";
+import MaxWrapper from "@/components/shared/MaxWrapper";
+
 export default function About() {
-    return <main className="min-h-screen flex flex-col">About</main>;
+    return <main className="min-h-screen flex flex-col">
+        <MaxWrapper className="flex flex-col w-full">
+            {/* Hero section */}
+            <HeroSection />
+        </MaxWrapper>
+    </main>;
 }
