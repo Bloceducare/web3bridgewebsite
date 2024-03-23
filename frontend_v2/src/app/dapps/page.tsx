838615878b6bac61dbd7ed399239e923a031f2d7
JeffreyJoel <jowoloko@gmail.com>
2024-03-23 21:32:20 +0100

    begin dapp

---
+++ frontend_v2/src/app/dapps/page.tsx
@@ -0,0 +1,15 @@
+import Join from "@/components/events/Join";
+import MaxWrapper from "@/components/shared/MaxWrapper";
+
+
+export default function Dapps() {
+    return (
+        <main className="min-h-screen flex flex-col">
+            <MaxWrapper className="flex flex-col w-full">
+                {/* Hero section */}
+                <div></div>
+            </MaxWrapper>
+            <Join />
+        </main>
+    )
+}
