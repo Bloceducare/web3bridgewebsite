3d6904756f7e5bcca1bc980219c3f1eee01f6178
mubarakOke <okeolamubarak18@gmail.com>
2024-03-24 02:07:20 +0100

    set up model for testimonial

--- backend_v2/apps/cohort/models.py
+++ backend_v2/apps/cohort/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from apps.utils.helpers.models import BaseModelBaseMixin
 
-# Create your models here.
+# Model
 # Course model
 class Course(BaseModelBaseMixin, models.Model):  
 
@@ -15,8 +15,23 @@ class Registration(BaseModelBaseMixin, models.Model):
     def __str__(self):
         pass
     
+# Testimonial 
+# Testimonial image storage location
+def testimonial_image_location(instance, filename):
+    return f"Cohort/Testimonial/{instance.last_name}_{instance.first_name}/{filename}"
+    
 # Testimonial model
 class Testimonial(BaseModelBaseMixin, models.Model):  
-
+    headline= models.CharField(_('headline'), max_length=1000, blank=False, null=False)
+    last_name= models.CharField(_('last name'), max_length=255, blank=False, null=False)
+    first_name= models.CharField(_('first name'), max_length=255, blank=False, null=False)
+    testimony= models.TextField(_("testimony"), blank=False, null=False)
+    picture= models.ImageField(upload_to=testimonial_image_location, blank=False, null=False)
+    brief= models.CharField(_('author brief'), max_length=255, blank=False, null=False)
+    
     def __str__(self):
-        pass
+        return f"< {type(self).__name__}({self.last_name} {self.first_name}) >"
+    
+    
+        
+    
