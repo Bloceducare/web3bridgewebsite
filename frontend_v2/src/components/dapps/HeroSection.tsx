c678865d5e8ef0e09707f8134a3f39b771f0b78b
JeffreyJoel <jowoloko@gmail.com>
2024-03-24 15:32:07 +0100

    update dapp

--- frontend_v2/src/components/dapps/HeroSection.tsx
+++ frontend_v2/src/components/dapps/HeroSection.tsx
@@ -1,13 +1,18 @@
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
 import defi from "../../../public/dapps/defi.png";
 import gaming from "../../../public/dapps/gaming.png";
+import art from "../../../public/dapps/art.png";
+import Header from "../../../public/dapps/Header.svg";
+import headerBg from "../../../public/dapps/header-bg.png";
 
 import Pill from "../shared/pill";
+import { Button } from "../ui/button";
+import { MoveRight } from "lucide-react";
 
 export default function HeroSection() {
   return (
-    <main>
-      <div className="w-full flex lg:flex-row flex-col gap-4 md:gap-10 justify-between items-center py-6">
+    <main className=" pt-28  pb-6 ">
+      <div className="w-full flex lg:flex-row flex-col gap-4 md:gap-10justify-between items-center">
         <div className={`basis-1/2 text-[#1B1B1B] order-first`}>
           <Pill text="Decentralized Networks" />
           <h1 className="mb-4 mt-2 md:mt-5 text-xl md:text-4xl font-semibold leading-none tracking-tight">
@@ -18,25 +23,39 @@ export default function HeroSection() {
             Dapps are a growing movement of applications that use Ethereum to
             disrupt business models or invent new ones.
           </p>
-          <div className="flex mr-3">
-            <span className="flex items-center">
+          <div className="flex ">
+            <span className="flex items-center mr-10">
               <Image priority src={defi} alt="defi" className="mr-2" />{" "}
               Decentralized Finance
             </span>
-            <span className="flex items-center">
+            <span className="flex items-center mr-10">
               <Image priority src={gaming} alt="gaming" className="mr-2" />{" "}
               Gaming
             </span>
             <span className="flex items-center">
-              <Image priority src={defi} alt="defi" className="mr-2" />{" "}
-              Art and Collectibles
+              <Image priority src={art} alt="defi" className="mr-2" /> Art and
+              Collectibles
             </span>
           </div>
         </div>
 
-        <div className="">
-          {/* <Image priority src={image} alt="Story image" className="" /> */}
+        <div className="relative">
+          {/* <Image src={headerBg} alt="Story imag" className="absolute top-[-120px] right-20 md:w-[680px] "/> */}
+          <Image
+            priority
+            src={Header}
+            alt="Story image"
+            className="md:w-[580px] relative top-0"
+          />
         </div>
+      </div>
+      <div className="my-4 flex items-center gap-6">
+        <Button className="rounded-full px-12 py-6 border-2 ring-2 ring-red-300 dark:ring-red-500 border-red-500 dark:border-red-300 bg-red-500/10 text-bridgeRed hover:bg-transparent">
+          Register for Event <MoveRight className="w-5 h-5 ml-2 " />
+        </Button>
+        <Button className="rounded-full px-12 py-6 border-2 ring-2 ring-red-300 dark:ring-red-400 border-none bg-red-500/10 text-bridgeRed hover:bg-transparent">
+          Learn More
+        </Button>
       </div>
     </main>
   );
