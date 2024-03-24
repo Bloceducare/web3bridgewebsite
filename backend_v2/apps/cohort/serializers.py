d97a1f94b33487098b221e0dad71516b6e799a3f
mubarakOke <okeolamubarak18@gmail.com>
2024-03-24 18:44:01 +0100

    setup serializers for cohort

---
+++ backend_v2/apps/cohort/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from cohort import models
+
+class CourseSerializer:
+    pass
+
+class RegistrationSerializer:
+    pass
+
+class ParticipantSerializer:
+    pass
+
+class TestimonialSerializer:
+    pass
