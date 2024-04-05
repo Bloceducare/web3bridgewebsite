e98440d74fb67d4b0f1a186d862bd5bb28f8dee3
signor1 <emmanuelomemgboji@gmail.com>
2024-04-05 17:05:15 +0100

    feat: updated the landing page

--- frontend_v2/src/components/home/Web3.tsx
+++ frontend_v2/src/components/home/Web3.tsx
@@ -54,7 +54,7 @@ const Web3 = () => {
         }
     ]
     return (
-        <section className="w-full flex flex-col items-center md:gap-8 gap-8 justify-center radial-gradient lg:px-6 md:px-2 my-40">
+        <section className="w-full flex flex-col items-center md:gap-8 gap-8 justify-center radial-gradient lg:px-6 md:px-2 md:my-40 my-32">
             <div className="flex flex-col items-center gap-3 ">
                 <h1 className="font-semibold leading-tight lg:text-5xl md:text-3xl text-[1.72rem] text-center">The Web 3.0 Cohort</h1>
                 <p className="w-full md:w-[70%] text-muted-foreground text-center">In <span className="text-bridgeRed">16 weeks,</span> get everything you need to launch your career in Blockchain Development through our trainings that gives you the nitty gritty of experience through practical classes.</p>
