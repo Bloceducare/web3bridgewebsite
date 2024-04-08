acae5ce5fccc55079861329c6eb9fce166151145
signor1 <emmanuelomemgboji@gmail.com>
2024-04-08 17:19:33 +0100

    feat: added home navlink

--- frontend_v2/src/lib/utils.ts
+++ frontend_v2/src/lib/utils.ts
@@ -7,6 +7,10 @@ export function cn(...inputs: ClassValue[]) {
 
 export const navLinks = [
   {
+    name: "Home",
+    href: "/",
+  },
+  {
     name: "Trainings",
     href: "/trainings",
   },
@@ -129,63 +133,63 @@ export const TeamMembers = [
   {
     name: "Awosika Israel Ayodeji",
     role: "Founder, Program Manager",
-    image: "/about/Team1.png"
+    image: "/about/Team1.png",
   },
 
   {
     name: "Akinnusotu Temitayo",
     role: "Co-founder, Lead Dev/Mentor",
-    image: "/about/Team2.png"
+    image: "/about/Team2.png",
   },
   {
     name: "Katangole Allan",
     role: "Head, Technical Training",
-    image: "/about/Team3.png"
+    image: "/about/Team3.png",
   },
   {
     name: "Jeremiah Noah",
     role: "Lead dev/ Mentor",
-    image: "/about/Team4.png"
+    image: "/about/Team4.png",
   },
 
   {
     name: "Oke Kehinde",
     role: "Blockchain Developer/ Mentor",
-    image: "/about/Team5.png"
+    image: "/about/Team5.png",
   },
   {
     name: "Abimbola Adebayo",
     role: "Blockchain Developer/ Mentor",
-    image: "/about/Team6.png"
+    image: "/about/Team6.png",
   },
   {
     name: "Falilat Owolabi",
     role: "Blockchain Developer/ Mentor",
-    image: "/about/Team7.png"
+    image: "/about/Team7.png",
   },
   {
     name: "Ademola Kelvin",
     role: "Blockchain Developer/ Mentor",
-    image: "/about/Team8.png"
+    image: "/about/Team8.png",
   },
   {
     name: "Yetunde Ige",
     role: "Project Manager",
-    image: "/about/Team9.png"
+    image: "/about/Team9.png",
   },
   {
     name: "Michael Jerry",
     role: "Community/ Social Media Lead",
-    image: "/about/Team10.png"
+    image: "/about/Team10.png",
   },
   {
     name: "Billy Luedtke",
     role: "Advisor & Angel investor",
-    image: "/about/Team11.png"
+    image: "/about/Team11.png",
   },
   {
     name: "Marek Laskowski, PhD",
     role: "Advisor, Founder Blockchain.lab",
-    image: "/about/Team12.png"
-  }
-  ];
+    image: "/about/Team12.png",
+  },
+];
