ca88b64f308fb201fc5705cc2e2474daba2822a4
superman <goodnesskolapo@gmail.com>
2024-04-09 13:27:56 +0100

    Update settings.py
    Signed-off-by: superman <goodnesskolapo@gmail.com>

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -26,11 +26,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS= [
     "http://localhost:3001",
-    "http://localhost:3000", 
+    "http://localhost:3000",
+    "*",
 ]
 
 
