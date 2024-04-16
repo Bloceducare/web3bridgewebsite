790364e2ec53fbaf80c27c1dc38c9ab540714efc
mubarakOke <okeolamubarak18@gmail.com>
2024-04-16 02:43:07 +0100

    updated allowed origin lists

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS= [
     "http://localhost:3001",
     "http://localhost:3000",
-    "https://web3bridgewebsitebackend.onrender.com",
+    "https://websitev2-cyan.vercel.app/",
+    "https://www.web3bridge.com/",
 ]
 
 
