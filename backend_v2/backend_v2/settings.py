a03d55d220d800f8371f6cafbb597ee5ca0f94ec
superman <goodnesskolapo@gmail.com>
2024-05-06 20:33:40 +0100

    ...

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -207,3 +207,15 @@ SWAGGER_SETTINGS = {
         "Bearer": {"type": "apiKey", "name": "Authorization", "in": "header"}
     }
 }
+
+
+"""""email settings"""
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
