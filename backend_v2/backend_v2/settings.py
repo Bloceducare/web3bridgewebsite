797956981904c90a111414a413ca77aef3a97400
superman <goodnesskolapo@gmail.com>
2024-05-07 19:07:23 +0100

    omoh

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -27,8 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
-
-CORS_ALLOWED_ORIGINS= [
+CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://localhost:3000",
     "https://websitev2-cyan.vercel.app",
@@ -36,6 +35,7 @@ CORS_ALLOWED_ORIGINS= [
     "http://*",
     "https://*",
     "https://www.web3bridgeafrica.com",
+    "*",
 ]
 
 
