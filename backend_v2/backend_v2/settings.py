a3d7d71b19995041a469fac39091178f2717605f
superman <goodnesskolapo@gmail.com>
2024-06-06 09:35:18 +0100

    ...

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -28,17 +28,8 @@ DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3001",
-    "http://localhost:3000",
-    "https://websitev2-cyan.vercel.app",
-    "https://www.web3bridge.com",
     "http://*",
-    "https://www.web3bridgeafrica.com",
-    "https://web3bridgeafrica.com",
     "https://*",
-    "https://web3bridgeafrica.com",
-    "https://www.web3bridgeafrica.com",
-    "https://www.web3bridgeafrica.com",
 ]
 
 
