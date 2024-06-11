33ef8aa010e9c8204e39c9d685ef798fb23ccaf4
superman <goodnesskolapo@gmail.com>
2024-06-11 18:19:50 +0100

    backend setings update

--- backend_v2/backend_v2/settings.py
+++ backend_v2/backend_v2/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
