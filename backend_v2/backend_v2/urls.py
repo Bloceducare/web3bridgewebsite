9e304a9f2f58a764ec3437dd708bd9c55bbd0bad
Ijay_dev <inyangasi4@gmail.com>
2024-06-26 15:07:51 +0000

    fix typo on serializers

--- backend_v2/backend_v2/urls.py
+++ backend_v2/backend_v2/urls.py
@@ -37,6 +37,7 @@ urlpatterns = [
     path(f'api/{settings.API_VERSION}/cohort/', include('cohort.urls', namespace=settings.API_VERSION), name='cohort'),
     path(f'api/{settings.API_VERSION}/dapp/', include('dapp.urls', namespace=settings.API_VERSION), name='dapp'),
     path(f'api/{settings.API_VERSION}/operation/', include('operation.urls', namespace=settings.API_VERSION), name='operation'),
+    path(f'api/{settings.API_VERSION}/event/', include('event.urls'), namespace=settings.API_VERSION, name='event'),
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
