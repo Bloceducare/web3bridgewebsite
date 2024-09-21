253a2159b9ab6999300f9254caaa68f40a1749ad
obah <olusojiobah@gmail.com>
2024-09-21 11:47:29 +0100

    style: optimized mobile styles

--- frontend_v2/src/app/career/[id]/page.tsx
+++ frontend_v2/src/app/career/[id]/page.tsx
@@ -25,7 +25,7 @@ export default function JobPage({ params }: { params: { id: string } }) {
 
   return (
     <section className="mt-10 mb-40">
-      <div className="px-20 pb-24">
+      <div className="px-4 pb-10 sm:px-20 sm:pb-24">
         <Image
           src={imageUrl ? imageUrl : "/career/Announcements.svg"}
           alt="job image"
@@ -33,14 +33,14 @@ export default function JobPage({ params }: { params: { id: string } }) {
           height={400}
         />
 
-        <h1 className="text-center font-semibold text-[#1F1F1F] text-5xl leading-[60px] mt-16 mb-4">
+        <h1 className="text-center font-semibold text-[#1F1F1F] text-2xl sm:text-5xl sm:leading-[60px] mt-16 mb-4 dark:text-white">
           {title}
         </h1>
 
         <JobHighlight data={data} />
       </div>
 
-      <div className="bg-[#FFFBF3] py-24 px-52 space-y-10">
+      <div className="bg-[#FFFBF3] px-3 py-8 md:py-14 md:px-20 lg:py-24 lg:px-52 space-y-10">
         <ItemWrapper title="Company Overview" value={companyOverview} />
 
         <ItemWrapper title="Job Description" value={jobDescription} />
@@ -66,10 +66,12 @@ function ItemWrapper({
 }) {
   return (
     <div className="space-y-4">
-      <h2 className="font-semibold text-[32px] text-[#1F1F1F]">{title}</h2>
+      <h2 className="font-semibold text-xl text-center sm:text-left sm:text-[32px] text-[#1F1F1F]">
+        {title}
+      </h2>
 
       {Array.isArray(value) ? (
-        <ol>
+        <ol className="list-decimal space-y-2">
           {value.map((item) => (
             <li key={item} className="text-[#1C1C1C] text-base">
               {item}
--- frontend_v2/src/components/career/jobHighlight.tsx
+++ frontend_v2/src/components/career/jobHighlight.tsx
@@ -5,12 +5,13 @@ import { JobDescription } from "@/data/dummyJobDescription";
 export function JobHighlight({ data }: { data: JobDescription }) {
   const { title, workplaceType, duration, employmentTYpe } = data;
   return (
-    <div className="bg-[#FFF7F5] rounded-[36px] py-5 px-10 flex items-center justify-between">
+    <div className="bg-[#FFF7F5] rounded-[36px] py-3 px-4 lg:py-5 lg:px-10 grid grid-cols-2 gap-4 md:gap-0 md:flex items-center justify-between w-full lg:w-9/12 mx-auto">
       <ItemWrapper title="Role" value={title} />
       <ItemWrapper title="Workplace Type" value={workplaceType} />
       <ItemWrapper title="Duration" value={duration ? duration : "n/a"} />
       <ItemWrapper title="Employment" value={employmentTYpe} />
-      <Button variant={"bridgePrimary"}>
+
+      <Button variant={"bridgePrimary"} className="text-xs md:text-sm">
         Apply For Postion <MoveRight className="w-5 h-5 ml-2 " />{" "}
       </Button>
     </div>
@@ -19,9 +20,11 @@ export function JobHighlight({ data }: { data: JobDescription }) {
 
 function ItemWrapper({ title, value }: { title: string; value: string }) {
   return (
-    <div className="space-y-6">
-      <p className="text-gray-500">{title}</p>
-      <p className="text">{value}</p>
+    <div className="space-y-3 md:space-y-6">
+      <p className="text-gray-500 text-sm md:text-base">{title}</p>
+      <p className="text-[#1C1C1C] text-sm md:text-base font-semibold">
+        {value}
+      </p>
     </div>
   );
 }
