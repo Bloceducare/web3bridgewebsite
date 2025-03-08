6a6eb8e030ca9f1f0b4d729b22038e33d906bef6
oluwagbemiga <oluwagbemigagbangbola@gmail.com>
2025-03-08 02:10:56 +0100

    feat: adjust description truncation length in TestimonialCard component

--- frontend_v2/src/components/home/TestimonialCard.tsx
+++ frontend_v2/src/components/home/TestimonialCard.tsx
@@ -13,15 +13,15 @@ const TestimonialCard: React.FC<testimonialProps> = ({
   const toggleDescription = () => setShowFullDescription(!showFullDescription);
 
   const truncatedDescription =
-    description.length > 750 ? description.slice(0, 750) + "..." : description;
+    description.length > 500 ? description.slice(0, 500) + "..." : description;
 
   return (
     <main className="lg:px-44 md:px-10 px-2">
       <div className="w-full flex flex-col items-center gap-4 p-6 border-2 border-bridgeRed/20 dark:border-bridgeRed/40 bg-brdigeRed/10 rounded-xl">
-        <p className="text-center font-light">
+        <p className="text-center transition-all duration-300 font-light">
           {showFullDescription ? description : truncatedDescription}
         </p>
-        {description.length > 750 && (
+        {description.length > 500 && (
           <button onClick={toggleDescription} className="text-bridgeRed">
             {showFullDescription ? "Show less" : "Show more"}
           </button>
